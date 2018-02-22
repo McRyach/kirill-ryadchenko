71c1ff291d87e18ad70fadb35f5055fadf7b2409
McRyach <mcryach@gmail.com>
2018-02-21 19:15:58 -0500

    Correct Wheel with inserBefore super quick function

--- script.js
+++ script.js
@@ -5,6 +5,9 @@
     var viewport = document.getElementsByClassName('viewport')[0];
     var viewpixel = viewport.scrollTop;
     var carousel = document.getElementsByClassName('list')[0];
+    var lastIndex = (document.getElementsByClassName('item').length)-1;
+    var first;
+    var last;
     
     window.onload = function(){
         viewport.scrollTop = 2700;
@@ -16,6 +19,9 @@
         
         viewpixel = viewport.scrollTop;
         
+        first = document.getElementsByClassName('item')[0];
+        last = document.getElementsByClassName('item')[lastIndex];
+
         clearTimeout(isScrolling);
 
         isScrolling = setTimeout(function(){
@@ -23,25 +29,39 @@
 
             if(viewpixel > 3099){
                 console.log("Add At the Bottom");
+                carousel.insertBefore(first, last);
+                viewport.scrollTop = viewpixel-700;
+
                 //var carousel = document.getElementsByClassName('list')[0];
                 //carousel.lastChild.after(carousel.firstChild);
-                viewport.scrollTop = viewpixel-700;
-                carousel.lastChild.after(carousel.firstChild);
+                //viewport.scrollTop = viewpixel-700;
+                //carousel.lastChild.after(carousel.firstChild);
+                //carousel.appendChild(carousel.firstChild);
                 //viewpixel = (viewpixel-3099)+2700;
                 //viewport.scrollTop = viewpixel;
             }
 
             if(viewpixel < 1801){
                 console.log("Add At the Top");
+                carousel.insertBefore(last, first);
+                viewport.scrollTop = viewpixel +700;
                 //var carousel = document.getElementsByClassName('list')[0];
                 //viewpixel = 1801-(1801-viewpixel);
                 //carousel.prepend(carousel.lastChild);
                 //viewport.scrollTop = viewpixel+700;
                 //carousel.prepend(carousel.lastChild);
-                viewport.scrollTop = viewpixel+700;
-                carousel.prepend(carousel.lastChild);
+                //viewport.scrollTop = viewpixel+700;
+                //carousel.prepend(carousel.lastChild);
                 //viewpixel = 2700-(1801-viewpixel);
                 //viewport.scrollTop = viewpixel;
+            }
+
+            if(first == carousel.lastChild){
+                console.log("Added Bottom");
+            }
+
+            if(last == carousel.firstChild){
+                console.log("Added Top");
             }
 
         }, 66);
