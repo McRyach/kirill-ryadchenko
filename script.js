bd0cd9b3936c412aecd0686428b1cc2dfc72ef24
McRyach <mcryach@gmail.com>
2018-02-21 22:06:57 -0500

    Wheeling

--- script.js
+++ script.js
@@ -2,15 +2,16 @@
 (function(){
 
     var isScrolling;
-    var viewport = document.getElementsByClassName('viewport')[0];
-    var viewpixel = viewport.scrollTop;
-    var carousel = document.getElementsByClassName('list')[0];
+    //var viewport = document.getElementsByClassName('viewport')[0];
+    //var viewpixel = viewport.scrollTop;
+    var carousel = document.getElementById('wheel');
     var lastIndex = (document.getElementsByClassName('item').length)-1;
     var first;
     var last;
     
     window.onload = function(){
-        viewport.scrollTop = 2700;
+        //viewport.scrollTop = 2700;
+        window.scrollTo(0,2450);
     }
 
     function isTouchDevice(){
@@ -18,16 +19,18 @@
     }
 
     if(isTouchDevice()){
-        viewport.addEventListener("touchmove", scrollingEvent, {passive: true});
+        //viewport.addEventListener("touchmove", scrollingEvent, {passive: true});
+        window.addEventListener("touchmove", scrollingEvent, {passive: true});
     } else {
-        viewport.addEventListener("scroll", scrollingEvent, {passive: true});
+        //viewport.addEventListener("scroll", scrollingEvent, {passive: true});
+        window.addEventListener("scroll", scrollingEvent, {passive: true});
     }
     
     //viewport.addEventListener("scroll", scrollingEvent, {passive: true});
     
     function scrollingEvent() {
         
-        viewpixel = viewport.scrollTop;
+        viewpixel = window.scrollY;//viewport.scrollTop;
         document.getElementById('debuger').innerHTML = "Pixel: "+ viewpixel;
         first = document.getElementsByClassName('item')[0];
         last = document.getElementsByClassName('item')[lastIndex];
@@ -41,13 +44,15 @@
             if(viewpixel > 3099){
                 console.log("Add At the Bottom");
                 carousel.insertBefore(first, last);
-                viewport.scrollTop = viewpixel-700;
+                //viewport.scrollTop = viewpixel-700;
+                window.scroll(0, viewpixel-700);
             }
 
             if(viewpixel < 1801){
                 console.log("Add At the Top");
                 carousel.insertBefore(last, first);
-                viewport.scrollTop = viewpixel +700;
+                //viewport.scrollTop = viewpixel +700;
+                window.scroll(0,viewpixel+700);
             }
 
         }, 3);
