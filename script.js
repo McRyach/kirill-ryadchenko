74b5b469318ff1b63e8dad3c0c00d25222abebed
McRyach <mcryach@gmail.com>
2018-02-26 20:11:10 -0500

    Version Control

--- script.js
+++ script.js
@@ -6,7 +6,7 @@
     var lastIndex = document.getElementById('wheel').childElementCount;
     var first;
     var last;
-    var version = " V0.28 ";
+    var version = " V0.29 ";
     var deb = document.getElementById('debuger');
     var clientX, clientY;
     var scrollBack;
