2f573ba9057eb28e9c678ea1bd4a564fb42d7148
McRyach <mcryach@gmail.com>
2018-03-01 21:41:57 -0500

    No Webkit

--- script.js
+++ script.js
@@ -6,7 +6,7 @@
     var lastIndex = document.getElementById('wheel').childElementCount;
     var first;
     var last;
-    var version = " V0.36 ";
+    var version = " V0.37 ";
     var deb = document.getElementById('debuger');
     var clientX, clientY;
     var scrollBack;
